488bcece18799f1638fc92405e6715bbe90bde0c
Sarang <sarangahlawat73@gmail.com>
2024-07-09 17:57:21 +0530

    pop up edit and others

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//get elements
 let textBox = document.querySelector(".text");
 let buttons = document.querySelectorAll(".btn");
 
@@ -5,15 +6,19 @@ let result = "";
 
 buttons.forEach(btn => {
     btn.addEventListener('click', (e) => {
-        console.log("A button was clicked");
+        // console.log("A button was clicked");
+
+        //equal button function
         if (e.target.innerHTML == "=") {
             result = eval(result);
             textBox.value = result;
         }
+        //AC button function
         else if (e.target.innerHTML == "AC") {
             result = "";
             textBox.value = result;
         }
+        //other button function
         else {
             // console.log(e.target);
             result = result + e.target.innerHTML;
@@ -22,3 +27,12 @@ buttons.forEach(btn => {
     });
 });
 
+//on clicking equal button a message will pop up
+let eqlBtn = () => {
+    document.getElementById('eqlpara').textContent = "Press AC for new calculation";
+}
+
+//on clicking AC button pop up message will hide
+let acBtn = () => {
+    document.getElementById('eqlpara').textContent = "";
+}
